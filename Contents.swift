6f04728635813cbb06657816c61b637cac90a883
Gio <32712976+gptskialadze@users.noreply.github.com>
2025-02-02 15:56:50 +0400

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -1,131 +1,226 @@
-import UIKit
+//Task 1: Area Calculator
 
-//Task 1: Functions
-func factorial(n: Int?) -> Int? {
-    guard let n = n else {
-          return nil
-      }
-        if n < 0 {
-            print(n, "is negative")
-            return nil
-        }
-        if n == 0 || n == 1 {
-            return 1
-        }
-        if n > 1 {
-            if let recursiveResult = factorial(n: n - 1) {
-                return n * recursiveResult
-            } else {
-                return nil
-            }
+protocol Shape {
+    func area() -> Double
+}
+
+
+struct Rectangle: Shape {
+    let width: Double
+    let height: Double
+    func area() -> Double {
+        return width * height
     }
-    return nil
 }
-if let result = factorial(n: 5) {
-    print(result)
-}
-
-
-
-//Task 2: Working With Arrays and custom Structures
-
-struct Student {
-    var name: String
-    var grades: [Double]
-}
-
-func averageGrade(student: Student) -> Double? {
-    var sum: Double = 0
-    for i in student.grades {
-        sum += i
+struct Circle: Shape {
+    let radius: Double
+    func area() -> Double {
+        return .pi * radius * radius
     }
-    return sum / Double(student.grades.count)
 }
 
-func bestStudent(inClass: [String: Student]) -> (Student, Double) {
-    var greateGrade: Double = 0
-    var student: String = "Alice"
-    for (index, item) in inClass.enumerated() {
-        if greateGrade < averageGrade(student: item.value)! {
-            greateGrade = averageGrade(student: item.value)!
-            student = item.key
-        }
+extension Shape {
+    var description: String {
+        String(describing: type(of: self))
     }
-    return (inClass[student]!, greateGrade)
 }
 
 
-let students = [
-    "Alice" : Student(name: "Alice", grades: [5.5, 3.5, 5.75]),
-    "Bob" : Student(name: "Bob", grades: [5.25, 3.75, 5.75])
-]
+let rectangle = Rectangle(width: 5, height: 10)
+print(rectangle.area()) // Output: 50.0
+print(rectangle.description) // Output: Rectangle
 
-
-if let studentsGraderesult = averageGrade(student: students["Alice"]!) {
-    print(studentsGraderesult)
-}
-
-print(bestStudent(inClass: students))
+let circle = Circle(radius: 7)
+print(circle.area()) // Output: ~153.94
+print(circle.description) // Output: Circle
 
 
 
 
-//Task 3: Find the longest word in a list of words using recursion
-
-func findLongestWord(words: [String?]) -> String? {
-    var maxString: Int = 0
-    var word: String!
-    for item in words {
-        if maxString < (item?.count)! {
-            maxString = (item?.count)!
-            word = item
-        }
+//Task 2: Person and Employee
+class Person {
+    let name: String
+    let age: Int
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
     }
-    return word
+    func  greet() {
+        print("Hi, I'm \(name) and I'm \(age) years old.")
+    }
+
 }
 
-if let longWord = findLongestWord(words: ["Hello", "World", "Swift", "Programming"]) {
-    print(longWord)
+class Employee: Person {
+    let jobTitle: String
+    init(name: String, age: Int, jobTitle: String) {
+        self.jobTitle = jobTitle
+        super.init(name: name, age: age)
+    }
+    override func greet() {
+        print("Hi, I'm \(name), a \(jobTitle), and I'm \(age) years old.")
+    }
+}
+
+let person = Person(name: "Alice", age: 25)
+person.greet() // Output: Hi, I'm Alice and I'm 25 years old.
+
+let employee = Employee(name: "Bob", age: 30, jobTitle: "Engineer")
+employee.greet() // Output: Hi, I'm Bob, a Engineer, and I'm 30 years old.
+
+
+
+
+
+//Task 3: Power Consumption
+protocol Appliance {
+    var powerUsage: Double { get }
+    func calculateMonthlyConsumption(hours: Double) -> Double
 }
 
 
-//Task 4: Shopping List
-
-struct ShoppingItem {
-    var name: String
-    var quantity: Double
-    var isPurchased: Bool = false
-}
-
-struct ShoppingList {
-    var items: [String: ShoppingItem] = [:]
+struct  WashingMachine: Appliance {
+    var powerUsage: Double = 500
     
-    mutating func addItem(item: ShoppingItem)-> Void {
-        self.items = [item.name: item]
+    func calculateMonthlyConsumption(hours: Double) -> Double {
+        return hours * powerUsage
     }
     
-    mutating func markAsPurchased(item: ShoppingItem) {
-        self.items[item.name]?.isPurchased = true
-    }
     
-    mutating func markItemAsPurchased(itemName: String){
-        if let item = self.items[itemName] {
-            self.items[itemName]?.isPurchased = true
-        }
-    }
+}
+struct Refrigerator: Appliance {
+    var powerUsage: Double = 1000
     
-    func listUnpurchasedItems() -> [ShoppingItem] {
-         var unParchised = items.filter({ (key: String, value: ShoppingItem) in
-             value.isPurchased == false
-        })
-        return Array(unParchised.values)
+    func calculateMonthlyConsumption(hours: Double) -> Double {
+        return hours * powerUsage
     }
 }
 
-var shoppingList = ShoppingList()
-shoppingList.addItem(item: ShoppingItem(name: "Apples", quantity: 5))
-shoppingList.addItem(item: ShoppingItem(name: "Bananas", quantity: 3))
-shoppingList.markItemAsPurchased(itemName: "Apples")
+extension Appliance {
+    func calculateMonthlyConsumption(hours: Double = 1000) -> Double {
+        return powerUsage * hours
+    }
+}
 
-let unpurchased = shoppingList.listUnpurchasedItems()
-print(unpurchased)
+extension Appliance {
+    var description: String {
+        return "This appliance uses \(powerUsage) watts each month."
+    }
+}
+
+let washingMachine = WashingMachine()
+print(washingMachine.description) // Output: This appliance uses 500 watts.
+print(washingMachine.calculateMonthlyConsumption(hours: 30)) // Output: 15,000 watts
+
+let refrigerator = Refrigerator()
+print(refrigerator.description) // Output: This appliance uses 100 watts.
+print(refrigerator.calculateMonthlyConsumption(hours: 720)) // Output: 72,000 watts
+
+
+
+
+
+
+
+//Task 4: Library
+struct Book: Equatable {
+    let title: String
+    let author: String
+  
+}
+
+protocol Borrowable {
+    func borrow()
+    func returnBook()
+}
+
+
+class Library: Borrowable {
+    var books: [Book] = []
+    let book = Book(title: "1984", author: "George Orwell")
+    
+    func borrow() {
+        if let index = books.firstIndex(of: self.book) {
+            books.remove(at: index)
+        }
+    }
+    
+    func returnBook() {
+        books.append(self.book)
+    }
+    
+    func addBook(book: Book) {
+        books.append(book)
+    }
+}
+
+
+
+extension Library {
+    func listBooks() {
+        print(books)
+    }
+}
+
+var library = Library()
+library.addBook(book: Book(title: "1984", author: "George Orwell"))
+library.addBook(book: Book(title: "To Kill a Mockingbird", author: "Harper Lee"))
+
+library.listBooks()
+// Output:
+// 1. 1984 by George Orwell
+// 2. To Kill a Mockingbird by Harper Lee
+
+library.borrow()
+// Output: Borrowed 1984
+library.listBooks()
+// Output: 1. To Kill a Mockingbird by Harper Lee
+
+
+
+
+
+
+
+//Task 5: Vehicles
+
+
+class Vehicle  {
+    let make: String
+    let model: String
+    let fuelEfficiency: Double
+    
+    init(make: String, model: String, fuelEfficiency: Double) {
+        self.make = make
+        self.model = model
+        self.fuelEfficiency = fuelEfficiency
+    }
+    
+    func calculateFuelNeeded(for distance: Double) -> Double {
+        return distance / fuelEfficiency
+    }
+
+}
+
+class ElectricCar: Vehicle {
+    var batteryCapacity: Double
+    init(make: String, model: String, batteryCapacity: Double) {
+        self.batteryCapacity = batteryCapacity
+        super.init(make: make, model: model, fuelEfficiency: batteryCapacity)
+    }
+    override func calculateFuelNeeded(for distance: Double) -> Double {
+        return distance / batteryCapacity
+    }
+}
+
+extension Vehicle {
+    func description() {
+        print("\(make) and \(model)")
+    }
+}
+
+let car = Vehicle(make: "Toyota", model: "Corolla", fuelEfficiency: 15)
+print(car.calculateFuelNeeded(for: 150)) // Output: 10.0 liters
+
+let tesla = ElectricCar(make: "Tesla", model: "Model 3", batteryCapacity: 50)
+print(tesla.calculateFuelNeeded(for: 150)) // Output: 30.0 kWh
